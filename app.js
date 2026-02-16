4731e8a3c5177e75499d33fb7343928ef44ce595
Sudam Fernando <sudam17fernando@gmail.com>
2026-02-16 14:21:26 +0530

    Fix console output for collaborator names display

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ console.log("SE3040 Lab03 - NodeJS Running Successfully");
 
 //Displaying the collaborators' names in the console
 console.log("Collaborators: PATHIRANA S.V(IT23555358) & Adeesha Sandaruwan");
+
+//Displaying the collaborators' names in the console
+console.log("Collaborators: FERNANDO D.S.N(IT23561120) & Adeesha Sandaruwan");
